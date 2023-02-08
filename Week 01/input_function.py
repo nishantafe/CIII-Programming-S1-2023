155ac6b059652b0841cfb4e0ef3ae1993ff1619d
Nishan <nishantafe@gmail.com>
2023-02-08 15:56:51 +1100

    Initial commit

---
+++ Week 01/input_function.py
@@ -0,0 +1,9 @@
+# input gets the value from a user and saves it as a String
+name = input("Please enter your name: ")
+
+# String must be converted to a number (integer) to be used for calculations
+age = int(input("Please enter your age: "))
+
+# Test
+print("Hello", name, "you are", age, "next year you'll be", age + 1)
+
---
+++ Week 01/intro.py
@@ -0,0 +1,13 @@
+print("Hello World")
+# This is a variable
+name = "Nishan"
+# String is a data type that should always be surrounded by double quotes "string"
+# We can combine a variable and a string in the print() function by separating them using a comma
+print("I hope you're having a nice day", name, ".")
+
+# Another data type is Numeric (no quotes needed)
+age = 70
+print(name, "is", age)
+
+
+
