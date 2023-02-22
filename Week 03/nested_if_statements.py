12f9d4cf992bc93866aa576fe99fac7697e1521c
Nishan <nishantafe@gmail.com>
2023-02-22 13:43:08 +1100

    Initial commit

---
+++ Week 03/nested_if_statements.py
@@ -0,0 +1,11 @@
+result = 50
+if result >= 50:
+    # it's 50 or above
+    if result == 50:
+        print("Well done")
+    else:
+        print("Excellent work")
+# it's 49 or less
+else:
+    print("Unfortunately you didn't pass")
+
---
+++ Week 03/nested_if_statements1.py
@@ -0,0 +1,11 @@
+result = int(input("Enter your result: "))
+if result >= 50:
+    # it's 50 or above
+    if result == 50:
+        print("Well done")
+    else:
+        print("Excellent work")
+# it's 49 or less
+else:
+    print("Unfortunately you didn't pass")
+
